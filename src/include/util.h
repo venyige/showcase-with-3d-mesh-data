85a64dc649bdc1cf2557e5520c3fdc3bf8306344
Tibor Venyige <46565795+venyige@users.noreply.github.com>
2020-01-06 10:09:08 +0100

    Update util.h

--- src/include/util.h
+++ src/include/util.h
@@ -16,11 +16,13 @@ typedef struct trPar{
 //http://jsfiddle.net/PerroAZUL/zdaY8/1/
 static int ptInTriangle(Eigen::Vector3d& p, Eigen::Vector3d& p0, Eigen::Vector3d& p1, Eigen::Vector3d& p2) {
     double A = .5 * (-p1.y() * p2.x() + p0.y() * (-p1.x() + p2.x()) + p0.x() * (p1.y() - p2.y()) + p1.x() * p2.y());
-    double s= (p0.y() * p2.x() - p0.x() * p2.y() + (p2.y() - p0.y()) * p.x() + (p0.x() - p2.x()) * p.y()) ;
+    double s;
     double t;
     if(std::signbit(A)){
+        s = -(p0.y() * p2.x() - p0.x() * p2.y() + (p2.y() - p0.y()) * p.x() + (p0.x() - p2.x()) * p.y()) ;
         t = -(p0.x() * p1.y() - p0.y() * p1.x() + (p0.y() - p1.y()) * p.x() + (p1.x() - p0.x()) * p.y()) ;
     }else{
+        s = (p0.y() * p2.x() - p0.x() * p2.y() + (p2.y() - p0.y()) * p.x() + (p0.x() - p2.x()) * p.y()) ;
         t = (p0.x() * p1.y() - p0.y() * p1.x() + (p0.y() - p1.y()) * p.x() + (p1.x() - p0.x()) * p.y()) ;
     }
     return s > 0. && t > 0. && (s + t) < 2. * abs(A);
